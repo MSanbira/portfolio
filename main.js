291bfe9674e550ba8999efb504802d4d7dff6917
MSanbira <myt.comp@gmail.com>
2018-07-28 13:23:01 +0300

    hamburger manu & bug fix

--- main.js
+++ main.js
@@ -1,25 +1,55 @@
 
 const sections = document.querySelectorAll('.wrapper');
 
+const headerNav = document.querySelector('.primary-nav');
+
 const navBtns = document.querySelectorAll('.nav-btn');
+
+const backdrop = document.querySelector('.backdrop');
 
 const curtain = document.querySelector('.curtain');
 
 const footer = document.querySelector('footer');
 
+const hamburger = document.querySelector('.hamburger');
+
 function openCurtain() {
     curtain.classList.add('open');
-    setTimeout(function () {curtain.classList.add('hide');}, 1000);
+    setTimeout(function () { curtain.classList.add('hide'); }, 1000);
 }
 
 function closeCurtain() {
     curtain.classList.remove('hide');
-    setTimeout(function () {curtain.classList.remove('open');}, 0);
+    setTimeout(function () { curtain.classList.remove('open'); }, 0);
+}
+
+function hideBackdrop() {
+    backdrop.classList.remove('active');
+    setTimeout(function () { backdrop.classList.add('hide'); }, 1000);
+}
+
+function showBackdrop() {
+    backdrop.classList.remove('hide');
+    setTimeout(function () { backdrop.classList.add('active'); }, 0);
 }
 
 function changeLocationHash(dataNav) {
+    if (dataNav != 'home' || hamburger.classList.contains('active')) {
+        openCloseNav();
+    }
     window.location.hash = dataNav;
     window.onhashchange = showSection();
+}
+
+function openCloseNav() {
+    if (hamburger.classList.contains('active')) {
+        hideBackdrop();
+    }
+    else {
+        showBackdrop();
+    }
+    hamburger.classList.toggle('active');
+    headerNav.classList.toggle('open');
 }
 
 function showSection() {
@@ -41,7 +71,7 @@ function showSection() {
         }
         footer.classList.remove('hide');
         openCurtain();
-    }, 1000,);
+    }, 1000, );
 }
 
 function init() {
@@ -60,15 +90,22 @@ function registerEvents() {
         if (event.target.classList.contains('nav-btn')) {
             changeLocationHash(event.target.getAttribute('data-nav'));
         }
+
+        if (event.target.classList.contains('hamburger') || event.target.parentElement.classList.contains('hamburger')) {
+            openCloseNav();
+        }
+        if (event.target.classList.contains('backdrop')) {
+            openCloseNav();
+        }
     });
 }
 
 window.addEventListener('scroll', (event) => {
     if (window.scrollY > 10) {
-        document.querySelector('.primary-header').classList.add('fixed-shadow');
+        document.querySelector('.primary-header').classList.add('header-scroll');
     }
     else {
-        document.querySelector('.primary-header').classList.remove('fixed-shadow');
+        document.querySelector('.primary-header').classList.remove('header-scroll');
     }
 });
 
